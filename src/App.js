e2749076fd95e552465b5f50e1c47530a8174e8b
digambrahma <digambrahma0@gmail.com>
2021-05-12 23:53:11 +0530

    changed readme file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App vertical-center">
+      
+
+      <div className="container" >
+        <div className="header">
+          <h1 >
+            Welcome to <a href="https://covvaccine.com">Covvaccine!</a>
+          </h1> 
+        </div>
+        <div>
+          <Form />  
+          </div>       
+        <div className="footer">
+          <p>
+            Developed by Digambar @2021  
+          </p>
+        </div>
+        </div>
     </div>
   );
 }
